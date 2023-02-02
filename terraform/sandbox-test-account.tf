7661730da15c5c30e9f3f86fd85c26134ffb4e36
Gabriel Guiridlian (EMIDS) <gabrielg@emids.com>
2023-02-02 15:21:33 -0300

    add sandbox-test-account

--- terraform/sandbox-test-account.tf
+++ terraform/sandbox-test-account.tf
@@ -3,7 +3,7 @@ module "aftsandbox" {
 
   control_tower_parameters = {
     AccountEmail              = "sindhuja.soma@gainwelltechnologies.com"
-    AccountName               = "aftsandbox1"
+    AccountName               = "aftsandboxtest"
     ManagedOrganizationalUnit = "GWT-AWS-PRODENG-GBL-OU-SANDBOX"
     SSOUserEmail              = "sindhuja.soma@gainwelltechnologies.com"
     SSOUserFirstName          = "Sandbox"
