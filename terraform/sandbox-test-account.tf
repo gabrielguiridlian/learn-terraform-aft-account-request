cabbda6d01c4c7d92f9a5533bce5df735e852de8
Gabriel Guiridlian (EMIDS) <gabrielg@emids.com>
2023-02-02 15:05:20 -0300

    add sandbox-test-account

---
+++ terraform/sandbox-test-account.tf
@@ -0,0 +1,27 @@
+module "aftsandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sindhuja.soma@gainwelltechnologies.com"
+    AccountName               = "aftsandbox1"
+    ManagedOrganizationalUnit = "GWT-AWS-PRODENG-GBL-OU-SANDBOX"
+    SSOUserEmail              = "sindhuja.soma@gainwelltechnologies.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
