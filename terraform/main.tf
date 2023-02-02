cc26f1727d92f998cce806e4db61b1d3b2076899
Gabriel Guiridlian (EMIDS) <gabrielg@emids.com>
2023-02-02 09:41:33 -0300

    adding new account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,21 @@ module "test" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
+<<<<<<< HEAD
     AccountEmail              = "gabriel.guiridlian@gainwelltechnologies.com"
     AccountName               = "Demo"
     ManagedOrganizationalUnit = "GWT-AWS-PRODENG-GBL-OU-SANDBOX"
     SSOUserEmail              = "gabriel.guiridlian@gainwelltechnologies.com"
     SSOUserFirstName          = "Demo"
     SSOUserLastName           = "AftTest"
+=======
+    AccountEmail              = "sindhuja.soma@gainwelltechnologies.com"
+    AccountName               = "testaft"
+    ManagedOrganizationalUnit = "GWT-AWS-PRODENG-GBL-OU-SANDBOX"
+    SSOUserEmail              = "sindhuja.soma@gainwelltechnologies.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
+>>>>>>> 177eaf4 (adding testaft)
   }
 
   account_tags = {
